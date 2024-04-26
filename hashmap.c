6a96f4d5f1f3555536e36cb6fb439383bf3044bf
sofiamenacortes <163350424+sofiamenacortes@users.noreply.github.com>
2024-04-26 19:48:48 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -91,21 +91,22 @@ HashMap *createMap(long capacity) {
 
 void eraseMap(HashMap *map, char *key)
 
-/*{
+{ /*
   if (map == NULL || key == NULL)
     return;
   long posicion = hash(key, map->capacity);
-  while (map->buckets[posicion] != NULL)
+  while (map->buckets[posicion] != NULL) */
     
-}*/
+}
 
-Pair *searchMap(HashMap *map, char *key) /*{ 
-  if (map == NULL || key == NULL)
+Pair *searchMap(HashMap *map, char *key) { 
+  /*if (map == NULL || key == NULL)
     return NULL;
   long posicion = hash(key, map->capacity);
-  while (map->buckets[posicion] != NULL)
+  while (map->buckets[posicion] != NULL)*/
     
-  return NULL; }*/
+  return NULL;
+}
 
 Pair *firstMap(HashMap *map) { return NULL; }
 
