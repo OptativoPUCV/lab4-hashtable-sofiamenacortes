4a308fffdda09ab5859628e4885a2b7b0bff16c1
sofiamenacortes <163350424+sofiamenacortes@users.noreply.github.com>
2024-04-26 20:42:51 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -134,18 +134,19 @@ Pair *searchMap(HashMap *map, char *key) {
 
   return currentpair;
 }
-
 Pair *firstMap(HashMap *map) { 
-  if (map == NULL)
-    return NULL;
+    if (map == NULL)
+        return NULL;
 
-  for(long i = 0; i < map->capacity; i++){
-    if(map->buckets[i] != NULL)
-      map->current = i;
-      return map->buckets[i];
-  }
-  return NULL; 
+    for (long i = 0; i < map->capacity; i++) {
+        if (map->buckets[i] != NULL) {
+            map->current = i; 
+            return map->buckets[i];
+        }
+    }
+    return NULL; 
 }
+
 
 Pair *nextMap(HashMap *map) { 
   if (map == NULL || map->current == -1)
